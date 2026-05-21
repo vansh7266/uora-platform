2e0997e865a8151d8ccbe24c91eb5462482f5c6d
vansh7266 <vanshgupta7266@gmail.com>
2026-05-21 20:49:55 +0530

    infra: enhance terraform variables and outputs for cloud resources

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,9 +1,14 @@
-output "public_ip" {
-  description = "Public IP of the UORA node"
-  value       = aws_instance.uora_node.public_ip
+output "cluster_name" {
+  description = "EKS cluster name"
+  value       = module.eks.cluster_name
 }
 
-output "ssh_command" {
-  description = "Command to SSH into the node"
-  value       = "ssh -i ~/.ssh/${var.key_name}.pem ubuntu@${aws_instance.uora_node.public_ip}"
+output "cluster_endpoint" {
+  description = "EKS cluster endpoint"
+  value       = module.eks.cluster_endpoint
+}
+
+output "configure_kubectl" {
+  description = "Command to configure kubectl for the UORA EKS cluster"
+  value       = "aws eks update-kubeconfig --region ${var.aws_region} --name ${module.eks.cluster_name}"
 }
